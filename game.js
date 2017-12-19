ecbe000339d0ddc377f7d849b0ff10fe912e77fe
tanvirovi <rahman.tanvir.ovi@gmail.com>
2017-12-19 21:07:10 +0100

    Adding new png files for every contributor use and adding needed comments to the code for better understanding

--- game.js
+++ game.js
@@ -2,23 +2,39 @@ var game;
 
 window.onload = function() {	
 	game = new Phaser.Game(640, 960, Phaser.AUTO, "");
+	//Boot state: in the boot state we will make all adjustment to the game to be resized accordingly to browser 
+	//resolution and aspect ratio
+				// name given(key) , function (state)
      game.state.add("Boot", boot);
-     game.state.add("Preload", preload); 
+	 
+	//we will use this state to preload all assets we will use in the game.
+	//It's the classic “loading” screen you see in most games 
+     game.state.add("Preload", preload);
+	
+	//the title screen, showing your game name and a play button.
      game.state.add("TitleScreen", titleScreen);
+	
+	//The game itself
      game.state.add("PlayGame", playGame);
+	
+	//The game over screen also features a “play again” button to let players restart the game
      game.state.add("GameOverScreen", gameOverScreen);
-     game.state.start("Boot");
+	 
+     game.state.start("Boot"); //starting the first state
 }
 
 var boot = function(game){};
 boot.prototype = {
+	
   	preload: function(){
-          this.game.load.image("loading","assets/sprites/loading.png"); 
+          this.game.load.image("loading","assets/sprites/loading.png"); //will load or show loading progress 
 	},
   	create: function(){
 		game.scale.pageAlignHorizontally = true;
 		game.scale.pageAlignVertically = true;
-          game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		// will set scaling method
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		// this will pass the state to the next state
 		this.game.state.start("Preload");
 	}      
 }
