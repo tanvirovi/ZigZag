d15e7de041ff5940b63bd6a75b3bafe1705670cf
fatema010 <34546964+fatema010@users.noreply.github.com>
2017-12-25 14:08:43 +0100

    adding game over state to the game

--- game.js
+++ game.js
@@ -159,5 +159,25 @@ playGame.prototype = {
 
 
 var gameOverScreen = function(game){};
-gameOverScreen.prototype = {    
+gameOverScreen.prototype = {
+    create: function(){
+        var titleBG = game.add.tileSprite(0, 0, game.width, game.height,
+        "backsplash");
+        titleBG.tint = bgColors[game.rnd.between(0, bgColors.length - 1)];
+        game.add.bitmapText(game.width / 2, 50 , "font", "Your score",
+        48).anchor.x = 0.5;
+        game.add.bitmapText(game.width / 2, 150 , "font", score.toString(),
+        72).anchor.x = 0.5;
+        var playButton = game.add.button(game.width / 2, game.height - 150,
+        "playbutton", this.startGame);
+        playButton.anchor.set(0.5);
+        var tween = game.add.tween(playButton).to({
+        width: 220,
+        height:220
+        }, 1500, "Linear", true, 0, -1);
+        tween.yoyo(true);
+        },
+        startGame: function(){
+        game.state.start("PlayGame");
+    }
 }
